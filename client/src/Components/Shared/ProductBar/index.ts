5a6805690fd637fe0f6d53b4e592dc029cca48be
KG <acrkdduf@naver.com>
2021-07-13 18:51:52 +0900

    feat: ProductBar 컴퍼넌트 기본 마크업 구조 작성 [#2]

---
+++ client/src/Components/Shared/ProductBar/index.ts
@@ -0,0 +1,18 @@
+import Component from '../../../core/Component';
+import './styles';
+
+export default class ProductBar extends Component {
+  template() {
+    const { price } = this.$props;
+
+    return `
+      <div class="product-bar-details">
+        <div class="image-wrapper">
+          <img src="https://user-images.githubusercontent.com/48883344/125425011-27afda53-2637-4d27-be86-ee2f6ccd78ef.png" alt="좋아요 버튼" />
+        </div>
+        <span class="product-price">${price}</span>
+      </div>
+      <div class="button">BUTTON</div>
+    `;
+  }
+}
