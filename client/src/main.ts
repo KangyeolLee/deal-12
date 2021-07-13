f29826d8333b1d64faeaae3d862bbea926d9868e
KG <acrkdduf@naver.com>
2021-07-13 18:35:21 +0900

    feat: SalesProductDetail 라우터 추가 [#3]

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,13 +1,15 @@
+import './scss/index.scss';
+import { initRouter } from './lib/router';
 import Category from './Components/Category';
 import Home from './Components/Home';
-import { initRouter } from './lib/router';
-import './scss/index.scss';
+import SalesProductDetail from './Components/SalesProductDetail';
 
 const $app = document.querySelector('#app');
 const routes = [
   { path: '/', redirect: '/home' },
   { path: '/home', component: Home },
   { path: '/category', component: Category },
+  { path: '/post', component: SalesProductDetail },
   // { path: '/login', component: LoginPage },
   // { path: '/stores', component: StorePage },
 ];
