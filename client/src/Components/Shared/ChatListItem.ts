47abac74ab9cfc8f0ff3c71c4a6a30e14f7ac6a2
KangyeolLee <48883344+KangyeolLee@users.noreply.github.com>
2021-07-14 10:44:21 +0900

    [feature] 채팅 목록 페이지 기본 마크업 구조 작성 (#12)
    * feat: 채팅 목록 페이지 라우트 추가
    
    * feat: 채팅 목록 페이지 기본 마크업 구조 작성 [#3]
    
    * feat: 공통 컴포넌트 - 각 채팅 항목 컴포넌트 클래스 작성 [#2]
    
    * 기존 구조 변경으로 인한 구버전 파일 제거

--- client/src/Components/Shared/ChatListItem.ts
+++
@@ -1,9 +0,0 @@
-import Component from '../../core/Component';
-
-export default class ChatListItem extends Component {
-  template() {
-    return `
-            <div>ChatListItem</div>
-          `;
-  }
-}
--- client/src/main.ts
+++ client/src/main.ts
@@ -2,12 +2,14 @@ import './scss/index.scss';
 import { initRouter } from './lib/router';
 import Home from './Components/Home';
 import SalesProductDetail from './Components/SalesProductDetail';
+import Chatlist from './Components/ChatList/index';
 
 const $app = document.querySelector('#app');
 const routes = [
   { path: '/', redirect: '/home' },
   { path: '/home', component: Home },
   { path: '/post', component: SalesProductDetail },
+  { path: '/chat', component: Chatlist },
   // { path: '/login', component: LoginPage },
   // { path: '/stores', component: StorePage },
 ];
