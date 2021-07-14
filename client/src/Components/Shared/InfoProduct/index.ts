45b6b7eaaca46228d799502af1c122854470d63e
KG <acrkdduf@naver.com>
2021-07-14 17:39:54 +0900

    refactor: 함수형 컴포넌트 추후 확장성을 위해 클래스형으로 전환 [#18]

--- client/src/Components/Shared/InfoProduct/index.ts
+++ client/src/Components/Shared/InfoProduct/index.ts
@@ -1,14 +1,5 @@
-// import Component from '../../../core/Component';
-
-// export default class InfoProduct extends Component {
-//   template() {
-//     return `
-//             <div>InfoProduct</div>
-//           `;
-//   }
-// }
-
 import './styles';
+import Component from '../../../core/Component';
 
 interface PropsType {
   title: string;
@@ -16,19 +7,21 @@ interface PropsType {
   image: string;
 }
 
-export default function InfoProduct(props: PropsType) {
-  const { title, price, image } = props;
+export default class InfoProduct extends Component {
+  template() {
+    const { title, price, image } = this.$props;
 
-  return `
-    <div class="product-intro">
-      <div class="image-wrapper">
-        <img src="${image}" alt="상품 미리보기" />
+    return `
+      <div class="product-intro">
+        <div class="image-wrapper">
+          <img src="${image}" alt="상품 미리보기" />
+        </div>
+        <div class="product-intro__detail">
+          <h6 class="title">${title}</h6>
+          <p calss="price">${price}</p>
+        </div>
       </div>
-      <div class="product-intro__detail">
-        <h6 class="title">${title}</h6>
-        <p calss="price">${price}</p>
-      </div>
-    </div>
-    <div class="button">판매중</div>
-  `;
+      <div class="button">판매중</div>
+    `;
+  }
 }
