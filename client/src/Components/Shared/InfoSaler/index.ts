0d478fcd34ac384d26f7e881f78206c2f0a7b7dc
KG <acrkdduf@naver.com>
2021-07-14 17:56:26 +0900

    refactor: 함수형 컴포넌트에서 확장성을 위해 클래스형으로 전환

--- client/src/Components/Shared/InfoSaler/index.ts
+++ client/src/Components/Shared/InfoSaler/index.ts
@@ -1,18 +1,5 @@
-// import Component from '../../../core/Component';
-
-// export default class InfoSaler extends Component {
-//   template() {
-//     return `
-//       <p class="sub-title">판매자 정보</p>
-//       <div class="user-area">
-//         <p class="username">Username</p>
-//         <p class="location">역삼동</p>
-//       </div>
-//     `;
-//   }
-// }
-
 import './styles';
+import Component from '../../../core/Component';
 
 interface PropsType {
   info: string;
@@ -20,13 +7,17 @@ interface PropsType {
   location: string;
 }
 
-export default function InfoSaler(props: PropsType) {
-  const { info, name, location } = props;
-  return `
-    <p class="sub-title">${info}</p>
-    <div class="user-area">
-      <p class="username">${name}</p>
-      <p class="location">${location}</p>
-    </div>
-  `;
+export default class InfoSaler extends Component {
+  template() {
+    console.log(this.$props, this.$target);
+    const { info, name, location }: PropsType = this.$props;
+
+    return `
+      <p class="sub-title">판매자 정보</p>
+      <div class="user-area">
+        <p class="username">Username</p>
+        <p class="location">역삼동</p>
+      </div>
+    `;
+  }
 }
