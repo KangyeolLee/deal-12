a9fbf058d26bbb909cb3f1c57abd7811caddb60f
KG <acrkdduf@naver.com>
2021-07-14 17:35:26 +0900

    refator: 함수형 컴포넌트 추후 확장성을 위한 클래스형으로 전환 [#18]

--- client/src/Components/Shared/ChatListItem/index.ts
+++ client/src/Components/Shared/ChatListItem/index.ts
@@ -1,16 +1,6 @@
-// import Component from '../../../core/Component';
-
-// export default class ChatListItem extends Component {
-//   template() {
-//     const { username, timestamp, content, img } = this.$props;
-
-//     return `
-
-//     `;
-//   }
-// }
-
 import './styles';
+
+import Component from '../../../core/Component';
 
 interface ParamsType {
   username: string;
@@ -20,20 +10,27 @@ interface ParamsType {
   checked?: boolean;
 }
 
-export default function ChatListItem(chat: ParamsType) {
-  const { username, timestamp, content, img, checked } = chat;
-  return `
-    <div class="chat-list__item ${checked ? 'checked' : ''}">
-      <div class="user-section">
-        <div class="user-section__detail">
-          <h6 class="username">${username}</h6>
-          <p class="content">${content}</p>
-        </div>
-        <span class="timestamp">${timestamp}</span>
-      </div>
-      <div class="image-wrapper">
-        <img src="${img}" alt="상품 미리보기" />
-      </div>
-    </div>
-  `;
+export default class ChatListItem extends Component {
+  template() {
+    return `
+      ${this.$props
+        .map((chat: ParamsType) => {
+          return `
+            <div class="chat-list__item ${chat.checked ? 'checked' : ''}">
+              <div class="user-section">
+                <div class="user-section__detail">
+                  <h6 class="username">${chat.username}</h6>
+                  <p class="content">${chat.content}</p>
+                </div>
+                <span class="timestamp">${chat.timestamp}</span>
+              </div>
+              <div class="image-wrapper">
+                <img src="${chat.img}" alt="상품 미리보기" />
+              </div>
+            </div>
+        `;
+        })
+        .join('')}
+    `;
+  }
 }
