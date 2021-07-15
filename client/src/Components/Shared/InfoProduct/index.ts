cc95002fb9da1fd7aeb58b7c4f5eae947fb1822e
KG <acrkdduf@naver.com>
2021-07-16 03:05:18 +0900

    InfoProcut & InfoSaler 컴포넌트 관련 레이아웃 깨짐 현상 수정

--- client/src/Components/Shared/InfoProduct/index.ts
+++ client/src/Components/Shared/InfoProduct/index.ts
@@ -1,5 +1,7 @@
 import './styles';
 import Component from '../../../core/Component';
+import Status from './../Status/index';
+import ImgBox from './../ImgBox/index';
 
 interface PropsType {
   title: string;
@@ -9,19 +11,33 @@ interface PropsType {
 
 export default class InfoProduct extends Component {
   template() {
-    const { title, price, image }: PropsType = this.$props;
+    const { title, price } = this.$props as PropsType;
 
     return `
       <div class="product-intro">
-        <div class="image-wrapper">
-          <img src="${image}" alt="상품 미리보기" />
-        </div>
+        <div class="image-wrapper"></div>
         <div class="product-intro__detail">
           <h6 class="title">${title}</h6>
-          <p calss="price">${price}</p>
+          <p class="price">${price}</p>
         </div>
       </div>
-      <div class="button">판매중</div>
+      <div class="status-button"></div>
     `;
+  }
+
+  mounted() {
+    const { image } = this.$props as PropsType;
+    const $status = this.$target.querySelector('.status-button');
+    const $imageWrapper = this.$target.querySelector('.image-wrapper');
+
+    new Status($status as HTMLElement, {
+      text: '판매중',
+      readonly: true,
+    });
+
+    new ImgBox($imageWrapper as HTMLElement, {
+      imgType: 'small',
+      img: image,
+    });
   }
 }
