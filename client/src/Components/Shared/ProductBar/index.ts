2b7800c05cffa7bbd7c5a4a650e68004a7fa86eb
KG <acrkdduf@naver.com>
2021-07-15 21:29:19 +0900

    깨지는 컴포넌트 및 페이지에서 디자인 재 수정

--- client/src/Components/Shared/ProductBar/index.ts
+++ client/src/Components/Shared/ProductBar/index.ts
@@ -1,6 +1,8 @@
 import Component from '../../../core/Component';
 import './styles';
 import IconButton from './../IconButton';
+import Button from '../Button/index';
+import { $router } from '../../../lib/router';
 
 export default class ProductBar extends Component {
   template() {
@@ -8,20 +10,25 @@ export default class ProductBar extends Component {
 
     return `
       <div class="product-bar-details">
-        <div class="image-wrapper">
-          <img src="https://user-images.githubusercontent.com/48883344/125425011-27afda53-2637-4d27-be86-ee2f6ccd78ef.png" alt="좋아요 버튼" />
-        </div>
+        <div class="image-wrapper"></div>
         <span class="product-price">${price}</span>
       </div>
-      <div class="button">BUTTON</div>
+      <div class="button"></div>
     `;
   }
 
   mounted() {
     const $imageWrapper = this.$target.querySelector('.image-wrapper');
+    const $button = this.$target.querySelector('.button');
 
     new IconButton($imageWrapper as HTMLElement, {
       name: 'heart',
+    });
+
+    new Button($button as HTMLElement, {
+      buttonType: 'medium',
+      title: '채팅 목록 보기',
+      handleClick: () => $router.push('/chat'),
     });
   }
 }
