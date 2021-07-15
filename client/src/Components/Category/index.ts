4595af6e2d77d817a4a97318ef8cb2ecb0da1024
leejeongmin <danmin20@khu.ac.kr>
2021-07-15 21:48:54 +0900

    feat: 카테고리 수정

--- client/src/Components/Category/index.ts
+++ client/src/Components/Category/index.ts
@@ -5,19 +5,19 @@ import './styles.scss';
 
 const categories = [
   { id: 1, name: '디지털기기' },
-  { id: 2, name: '디지털기기' },
-  { id: 3, name: '디지털기기' },
-  { id: 4, name: '디지털기기' },
-  { id: 5, name: '디지털기기' },
-  { id: 6, name: '디지털기기' },
-  { id: 7, name: '디지털기기' },
-  { id: 8, name: '디지털기기' },
-  { id: 9, name: '디지털기기' },
-  { id: 10, name: '디지털기기' },
-  { id: 11, name: '디지털기기' },
-  { id: 12, name: '디지털기기' },
-  { id: 13, name: '디지털기기' },
-  { id: 14, name: '디지털기기' },
+  { id: 2, name: '생활/가전' },
+  { id: 3, name: '가구/인테리어' },
+  { id: 4, name: '게임/취미' },
+  { id: 5, name: '생활/가공식품' },
+  { id: 6, name: '스포츠/레저' },
+  { id: 7, name: '여성패션/잡화' },
+  { id: 8, name: '남성패션/잡화' },
+  { id: 9, name: '유아동' },
+  { id: 10, name: '뷰티/미용' },
+  { id: 11, name: '반려동물' },
+  { id: 12, name: '도서/티켓/음반' },
+  { id: 13, name: '식물' },
+  { id: 14, name: '기타 중고물품' },
 ];
 
 interface CategoryBtnProps {
@@ -27,7 +27,7 @@ interface CategoryBtnProps {
 
 class CategoryBtn extends Component {
   template() {
-    const { id, name }: CategoryBtnProps = this.$props;
+    const { name }: CategoryBtnProps = this.$props;
 
     return `<div class="category-btn"><div class="box"></div><div class="name">${name}</div></div>`;
   }
