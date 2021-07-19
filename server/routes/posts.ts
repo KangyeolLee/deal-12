52e85df693769b0cac9b4b6799b297aee1837459
KG <acrkdduf@naver.com>
2021-07-19 17:16:59 +0900

    포스트 관련 라우트 재설정

--- server/routes/posts.ts
+++ server/routes/posts.ts
@@ -12,13 +12,14 @@ postsRouter.post('/', PostController.createPost);
 // SELECT * FROM post limit 25 offset 1
 postsRouter.get(
   '/location/:locationId/category/:categoryId',
-  PostController.createPost
+  PostController.getPosts
 );
 postsRouter.get('/:postId', PostController.getPostById);
 postsRouter.put('/:postId', PostController.updatePost);
 postsRouter.put('/:postId/state', PostController.updatePostState);
 postsRouter.delete('/:postId', PostController.deletePost);
 postsRouter.post('/:postId/like', PostController.creatPostLike);
+postsRouter.get('/:postId/like', PostController.getPostLikesByUserId);
 postsRouter.delete('/:postId/like', PostController.deletePostLike);
 
 export default postsRouter;
