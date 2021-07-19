31aff4964ec24aecfccf63da907b58426af4c4e6
KG <acrkdduf@naver.com>
2021-07-19 21:02:57 +0900

    post 관련 service 로직 작성

--- server/services/post/PostService.ts
+++ server/services/post/PostService.ts
@@ -1,5 +1,5 @@
 import { execQuery } from '../../database/database';
-import { FIND_POST_BY_USER_NICKNAME } from '../../queries/post';
+import { CREATE_POST, DELETE_POST, FIND_POST_BY_USER_NICKNAME, UPDATE_POST, UPDATE_POST_STATE_BY_POSTID } from '../../queries/post';
 import { FIND_ALL_POSTS, FIND_POST_BY_POSTID, UPDATE_POST_VIEWCOUNT } from '../../queries/post';
 
 export type PostType = {
@@ -7,30 +7,43 @@ export type PostType = {
   location_id: number;
   category_id: number;
   content: string;
-  price: number;
+  view_count: number;
+  price?: number;
   seller_id: number;
   state: string;
+  thumbnail: string;
+  interest_count: number;
 };
 
+export type PostUpdateTYpe = {
+  post_id: number;
+  title?: string;
+  location_id?: number;
+  category_id?: number;
+  content?: string;
+  view_count?: number;
+  price?: number;
+  seller_id?: number;
+  state?: string;
+  thumbnail?: string;
+  interest_count?: number;
+}
+
 export const PostService = {
-  cratePost: ({
+  cratePost: async ({
     title,
     location_id,
     category_id,
+    view_count,
+    seller_id,
+    interest_count,
     content,
     price,
-    seller_id,
     state,
+    thumbnail,
   }: PostType) => {
-    return `
-    INSERT INTO post('title', 'location_id', 'category_id', 'content', 'price', 'seller_id', 'state') VALUES
-      (${title},
-      ${location_id},
-      ${category_id},
-      ${content},
-      ${price},
-      ${seller_id},
-      ${state});`;
+    const data = await execQuery(CREATE_POST({ title, location_id, category_id, view_count, content, price, seller_id, state, thumbnail, interest_count }));
+    return data;
   },
 
   findPosts: async ({ location_id, category_id }: { location_id : number, category_id?: number }) => {
@@ -52,14 +65,16 @@ export const PostService = {
     const data = await execQuery(UPDATE_POST_VIEWCOUNT({ post_id }));
     return data;
   },
-  updatePost: async (post_id: number) => {
-    // const data = await execQuery(UPDATE_POST_VIEWCOUNT({ post_id }));
-    // return data;
+  updatePost: async ({post_id, title, price, content, location_id, category_id, state, thumbnail}: PostUpdateTYpe) => {
+    const data = await execQuery(UPDATE_POST({post_id, title, price, content, location_id, category_id, state, thumbnail}));
+    return data;
   },
-  updatePostState: () => {
-    return `UPDATE post SET state={asdf} WHERE id={postId};`;
+  updatePostState: async ({ post_id, state }: { post_id: number, state: string}) => {
+    const data = await execQuery(UPDATE_POST_STATE_BY_POSTID({ post_id, state }));
+    return data;
   },
-  deletePost: () => {
-    return `DELETE FROM post WHERE id={postId};`;
+  deletePost: async (post_id: number) => {
+    const data = await execQuery(DELETE_POST({post_id}));
+    return data;
   },
 };
