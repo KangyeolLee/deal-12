9279e4ff52cd7e1f879003ffa522e4a31ee17aa5
KG <acrkdduf@naver.com>
2021-07-20 14:58:12 +0900

    IconButton props 추가 : disabled / hidden

--- client/src/Components/Shared/IconButton/index.ts
+++ client/src/Components/Shared/IconButton/index.ts
@@ -4,14 +4,16 @@ import './styles.scss';
 interface IconButtonProps {
   name: string;
   small?: boolean;
+  disabled?: boolean;
+  hidden?: boolean;
 }
 
 export default class IconButton extends Component {
   template() {
-    const { name, small }: IconButtonProps = this.$props;
+    const { name, small, disabled, hidden }: IconButtonProps = this.$props;
 
-    return `<div class="${
-      small ? 'icon-btn small' : 'icon-btn'
-    }" style="background-image: url(../../../../assets/${name}.svg)"></div>`;
+    return `<div class="icon-btn${
+      small ? ' small' : ''
+    }${disabled ? ' disabled' : ''}${hidden ? ' hidden' : ''}" style="background-image: url(../../../../assets/${name}.svg)"></div>`;
   }
 }
