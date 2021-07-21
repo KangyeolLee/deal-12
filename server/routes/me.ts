8e93a83cd84faa78c9b730f9772f380df00133bf
leejeongmin <danmin20@khu.ac.kr>
2021-07-21 21:01:17 +0900

    feat: user api 추가

--- server/routes/me.ts
+++ server/routes/me.ts
@@ -4,25 +4,25 @@ import { PostController } from '../controllers/PostController';
 import { UserController } from '../controllers/UserController';
 import { authenticateAccessToken } from '../middlewares/authenticate';
 
-const usersRouter = express.Router();
+const meRouter = express.Router();
 
-usersRouter.get('/', authenticateAccessToken, UserController.getUserByNickname);
+meRouter.get('/', authenticateAccessToken, UserController.getUserByNickname);
 
 // @ GET 요청
 // 로그인 한 유저의 관심 포스트 목록
-usersRouter.get(
+meRouter.get(
   '/like/posts',
   authenticateAccessToken,
   PostController.getPostInterestsByUserNickname
 );
 // 로그인 한 유저가 작성한 포스트 목록
-usersRouter.get(
+meRouter.get(
   '/posts',
   authenticateAccessToken,
   PostController.getPostBySellerNickname
 );
 // 로그인 한 유저의 위치 목록
-usersRouter.get(
+meRouter.get(
   '/locations',
   authenticateAccessToken,
   LocationController.getLocationsByUserNickname
@@ -30,10 +30,6 @@ usersRouter.get(
 
 // @ PUT 요청
 // 로그인 한 유저의 위치 정보 수정
-usersRouter.put(
-  '/locations',
-  authenticateAccessToken,
-  UserController.updateUser
-);
+meRouter.put('/locations', authenticateAccessToken, UserController.updateUser);
 
-export default usersRouter;
+export default meRouter;
