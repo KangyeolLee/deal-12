89605a9a520dd00097e1d2c6336dae964fa6d988
KG <acrkdduf@naver.com>
2021-07-22 11:45:47 +0900

    ChatBadge 컴포넌트 폴더 생성 후 위치 조정

---
+++ client/src/Components/Shared/ChatBadge/ChatBadge.ts
@@ -0,0 +1,9 @@
+import Component from '../../../core/Component';
+
+export default class ChatBadge extends Component {
+  template() {
+    return `
+            <div>ChatBadge</div>
+          `;
+  }
+}
