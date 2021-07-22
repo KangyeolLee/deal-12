f67ddff6b66b9d31a6fdce312ca9268d4ce2b4fe
KG <acrkdduf@naver.com>
2021-07-22 18:26:42 +0900

    HOST address added

--- client/src/main.ts
+++ client/src/main.ts
@@ -9,7 +9,7 @@ import Location from './Components/Location';
 import { io } from 'socket.io-client';
 import UpdatePost from './Components/UpdatePost';
 
-export const socket = io(process.env.HOST as string);
+export const socket = io('http://3.34.200.100:3000');
 
 const $app = document.querySelector('#app');
 const routes = [
